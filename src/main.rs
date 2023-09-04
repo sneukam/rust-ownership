d5c618fa37f67f2d48cc6c9457cdba4e410464c8
Spencer Neukam <neukams@oregonstate.edu>
2023-09-04 17:46:37 -0400

    Played around with implicit ownership changes and the copy() trait(?)/function/method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("ownership!");
+    let mut s: String = String::from("hello");
+    s.push_str(", world!");  // push_str() appends a string literal to a String
+    println!("{}", s);
+
+    let sx: String = String::from("hello2");
+    let sj = sx;
+    // println!("error: sx = {sx}");   // error here because sj now has ownership, and sx is invalid.
+    // sj now owns the variable, and it alone will free the memory ('drop' in Rust) when it goes out of scope. There will be no double free of memory.
+    // Rust never creates deep copies of the data by default. sj points to the same locaiton in memory as sx.
+
+    // however, we can manually clone (deep copy).
+    // we cannot clone sx because it is no longer valid, its value is now owned by sj.
+    let sn: String = sj.clone();
+    println!("sn has been cloned (deep copied). sn = {sn}");
 }
